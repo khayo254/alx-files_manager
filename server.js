62b1f6c295bf5018f713acfadccef93d274aa5b5
khayo254 <khayo1998@gmail.om>
2024-07-03 23:40:11 +0300

    eslint corrections

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-// sets all the routes created in routes/index
 app.use('/', allRoutes);
 
 app.listen(port);
